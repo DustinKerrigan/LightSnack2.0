39c002da71dd1948fedbee30a894d77f685129bd
DustinKerrigan <dustindkerrigan245@gmail.com>
2025-10-20 13:19:31 -0400

    new endpoint link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
         const handleSubmit = async (e) => {
         e.preventDefault();
         const data = new FormData(e.target);
-        await fetch('http://34.134.148.220:5000/api/contact', {
+        await fetch('https://donation-nashville-quoted-intensive.trycloudflare.com/api/contact', {
             method: 'POST',
             body: JSON.stringify({
             firstName: data.get('firstName'),
